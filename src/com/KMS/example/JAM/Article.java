e2b488025e275ef23865c71eff7f279cb5f5cc4a
casatoo <casato6666@gmail.com>
2022-08-08 16:48:47 +0900

    bulid: article write ,list

--- src/com/KMS/example/JAM/Article.java
+++ src/com/KMS/example/JAM/Article.java
@@ -1,7 +1,22 @@
 package com.KMS.example.JAM;
 
-public class Article {
+import java.util.Date;
+
+public class Article extends Object{
 	int id;
 	String title;
 	String body;
+	Article(){		
+	}
+	
+	public Article(int id,String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
 }
--- src/com/KMS/example/JAM/Main.java
+++ src/com/KMS/example/JAM/Main.java
@@ -1,10 +1,15 @@
 package com.KMS.example.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		List<Article> articles = new ArrayList<>();
+		int id = 1;
+		
 		while (true) {
 			System.out.printf("명령어 :");
 			String cmd = sc.nextLine().trim();
@@ -14,13 +19,30 @@ public class Main {
 				break;
 			}
 
-			switch (cmdBit[1]) {
-				case "write":
-				
+			switch (cmd) {
+				case "article write":
+					System.out.println("== 게시물 작성 ==");
+					System.out.printf("제목:");
+					String title = sc.nextLine();
+					System.out.printf("내용:");
+					String body = sc.nextLine();
+					Article article = new Article(id,title,body);
+					articles.add(article);
+					id++;
+					System.out.println(article);
 					continue;
-				case "list":
 					
+				case "article list":
+					if(articles.size()==0) {
+						System.out.println("게시물이 없습니다.");
+						continue;
+					}
+					System.out.println("번호 / 제목 ");
+					for(Article articlelist:articles) {
+						System.out.println(articlelist.id +"  "+ articlelist.title);
+					}
 					continue;
+					
 				
 			}
 
